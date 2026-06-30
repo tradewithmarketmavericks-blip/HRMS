c039e50be730210c0edae1aceff6e4fb28203132
tradewithmarketmavericks-blip <tradewithmarketmavericks@gmail.com>
2026-06-30 13:55:22 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,5 +6,5 @@
 // 3. Copy "Project URL" and "anon public" key below
 // ============================================================
 
-const SUPABASE_URL  = sb_publishable_1hSYhopIHRBvqh5f3FyQSg_3utE0aNj;
+const SUPABASE_URL  = https://qopidfjpwpvhoteukkui.supabase.co/rest/v1/;
 const SUPABASE_ANON_KEY = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFvcGlkZmpwd3B2aG90ZXVra3VpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI1MzM2NTcsImV4cCI6MjA5ODEwOTY1N30.7nNSj8lMD4ce2JavRACgiczarQA78nubY-ZOSW8SczI;
