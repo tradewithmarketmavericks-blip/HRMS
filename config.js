dc89a578d84158c588f4c4343db59b92d8ec0575
tradewithmarketmavericks-blip <tradewithmarketmavericks@gmail.com>
2026-07-16 10:13:13 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,4 +6,4 @@
 // ============================================================
 
 const SUPABASE_URL      = 'https://qopidfjpwpvhoteukkui.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFvcGlkZmpwd3B2aG90ZXVra3VpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA5NTI3NjEsImV4cCI6MjA2NjUyODc2MX0.3E3zfKCOWPTaUSTF2tI39I0rM5VNnSQvhC0r4gG3few';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFvcGlkZmpwd3B2aG90ZXVra3VpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI1MzM2NTcsImV4cCI6MjA5ODEwOTY1N30.7nNSj8lMD4ce2JavRACgiczarQA78nubY-ZOSW8SczI';
